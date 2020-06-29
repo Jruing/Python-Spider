d7cacb24a92ad96ceef62d97281088a27e248803
Jruing <1099201992>
2020-06-29 15:42:12 +0800

    新增加机器人功能接口

---
+++ Functional_interface.py
@@ -0,0 +1,29 @@
+# -*-coding:utf-8-*-
+import random
+import requests
+from flask import Flask,jsonify,request
+from lxml.html import etree
+
+app = Flask(__name__)
+
+# 成语接龙
+@app.route('/Idiom_Solitaire/', methods=['POST'])
+def Idiom_Solitaire():
+    """
+    :param keyword: 关键字为最后一个字
+    :return:
+    """
+    keyword = request.json()
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    }
+    response = requests.get(f"http://www.chengyujielong.com.cn/search/{keyword}", headers=header)
+    html = etree.HTML(response.text)
+    chengyu_list = html.xpath(
+        "//div[@class='panel panel-default'][1]/div[@class='panel-body']/ul[@class='list-inline hot']/li/a/text()")
+    return jsonify({"status":True,"data":random.choices(chengyu_list)[0]})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
