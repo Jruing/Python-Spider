b05a2f7c0187380dc42d45cf8ed7eed6b9db5d51
Jruing <1099201992>
2020-06-29 15:58:43 +0800

    修复成语接龙参数接收问题

--- Functional_interface.py
+++ Functional_interface.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8-*-
-import random
+import random,json
 import requests
 from flask import Flask,jsonify,request
 from lxml.html import etree
@@ -13,7 +13,8 @@ def Idiom_Solitaire():
     :param keyword: 关键字为最后一个字
     :return:
     """
-    keyword = request.json()
+    params = json.loads(request.data)
+    keyword = params['start_time']
     header = {
         "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
     }
